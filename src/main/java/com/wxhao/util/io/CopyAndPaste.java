82492495d121ee00ca5f2f0746bf66fdc6439b0a
wxhao <wxhao911@outlook.com>
2016-08-31 22:24:23 +0800

    test second commit

---
+++ src/main/java/com/wxhao/util/io/CopyAndPaste.java
@@ -0,0 +1,13 @@
+package com.wxhao.util.io;
+
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+
+public class CopyAndPaste {
+	public void actionPerformed(ActionEvent e) {   
+//		clipboard = getToolkit().getSystemClipboard(); //获得系统粘贴板   
+//		StringSelection textInfoSelected = new StringSelection(textInfo.getText()); //建立一个粘贴板内容实例.   
+//		clipboard.setContents(textInfoSelected, null); //将textInfoSelected加入到粘贴板中;   
+//		labelInfo.setText("The output message has copied to clipboard.");   
+	}   
+}
